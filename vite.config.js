e15483ccaca0d2f385ea7dd30bdcc37d3877d0ea
econicmedia <econicm@gmail.com>
2026-09-01 03:41:39 +0300

    chore: configure Sites deployment

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,23 @@
+import { cloudflare } from '@cloudflare/vite-plugin';
+import { sites } from '@openai/sites-vite-plugin';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  // TODO: Configure path aliases if needed
-  // resolve: {
-  //   alias: {
-  //     '@': '/src',
-  //     '@components': '/src/components',
-  //     '@pages': '/src/pages',
-  //     '@styles': '/src/styles',
-  //     '@utils': '/src/utils',
-  //   },
-  // },
+  plugins: [
+    react(),
+    sites(),
+    cloudflare({
+      config: {
+        name: 'server',
+        main: './worker/index.js',
+        compatibility_date: '2026-05-15',
+        assets: {
+          binding: 'ASSETS',
+          not_found_handling: 'single-page-application',
+        },
+      },
+    }),
+  ],
 });
 
